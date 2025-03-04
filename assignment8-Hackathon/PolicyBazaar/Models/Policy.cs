9b71030c7a058ff0e2c9483aa9ad3e1a63a0ba52
Rahul Suthar <90858008+Rahulcs27@users.noreply.github.com>
2025-03-04 16:08:47 +0530

    commit

---
+++ assignment8-Hackathon/PolicyBazaar/Models/Policy.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolicyBazaar.Models
+{
+    public enum PolicyType
+    {
+        Life,
+        Health,
+        Vehicle,
+        Property
+    }
+    public class Policy
+    {
+        public int PolicyId { get; set; }
+        public string PolicyHolderName { get; set; }
+        public PolicyType Type { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public Policy(int policyId, string policyHolderName, PolicyType type, DateTime startDate, DateTime endDate)
+        {
+            PolicyId = policyId;
+            PolicyHolderName = policyHolderName;
+            Type = type;
+            StartDate = startDate;
+            EndDate = endDate;
+        }
+
+        public bool IsActive()
+        {
+            return DateTime.Now >= StartDate && DateTime.Now<= EndDate;
+        }
+        public override string ToString()
+        {
+            return $"ID: {PolicyId}, Name: {PolicyHolderName}, Type: {Type}, " +
+                   $"Start: {StartDate.ToShortDateString()}, End: {EndDate.ToShortDateString()}, " +
+                   $"Active: {IsActive()}";
+        }
+
+    }
+}
---
+++ assignment8-Hackathon/PolicyBazaar/Repositories/PolicyRepository.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InsurancePolicyApp.Exceptions;
+using PolicyBazaar.Interfaces;
+using PolicyBazaar.Models;
+
+namespace PolicyBazaar.Repositories
+{
+    internal class PolicyRepository : IPolicyRepository
+    {
+        private List<Policy> policies = new List<Policy>();
+        public void AddPolicy()
+        {
+            Console.WriteLine("Enter Policy ID:");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            // Check if policy already exists
+            if (policies.Exists(p => p.PolicyId == id))
+            {
+                Console.WriteLine("Policy Already Exists");
+                return;
+            }
+            Console.Write("Enter Policy Holder Name: ");
+            string name = Console.ReadLine();
+
+            Console.Write("Enter Policy Type (Life, Health, Vehicle, Property): ");
+            PolicyType type = (PolicyType)Enum.Parse(typeof(PolicyType), Console.ReadLine(), true);
+
+            Console.Write("Enter Start Date (yyyy-MM-dd): ");
+            DateTime startDate = DateTime.Parse(Console.ReadLine());
+
+            Console.Write("Enter End Date (yyyy-MM-dd): ");
+            DateTime endDate = DateTime.Parse(Console.ReadLine());
+
+            policies.Add(new Policy(id, name, type, startDate, endDate));
+            Console.WriteLine("Policy added successfully!");
+        }
+        public void ViewPolicy()
+        {
+            if (policies.Count == 0)
+            {
+                Console.WriteLine("No policies found.");
+                return;
+            }
+
+            foreach (var policy in policies)
+            {
+                Console.WriteLine(policy);
+            }
+        }
+        public void SearchPolicyById()
+        {
+            Console.Write("Enter Policy ID to search: ");
+            int id = int.Parse(Console.ReadLine());
+
+            var policy = policies.Find(p => p.PolicyId == id);
+            if (policy != null)
+                Console.WriteLine(policy);
+            else
+                Console.WriteLine("Policy not found!");
+        }
+
+        public void UpdatePolicy()
+        {
+            Console.Write("Enter Policy ID to update: ");
+            int id = int.Parse(Console.ReadLine());
+
+            var policy = policies.Find(p => p.PolicyId == id);
+            if (policy == null)
+            {
+                Console.WriteLine("Policy not found!");
+                return;
+            }
+
+            Console.Write("Enter new Policy Holder Name: ");
+            policy.PolicyHolderName = Console.ReadLine();
+
+            Console.WriteLine("Policy updated successfully!");
+        }
+
+        public void DeletePolicy()
+        {
+            Console.Write("Enter Policy ID to delete: ");
+            int id = int.Parse(Console.ReadLine());
+
+            var policy = policies.Find(p => p.PolicyId == id);
+            if (policy == null)
+                throw new PolicyNotFoundException($"Policy with ID {id} not found.");
+
+            policies.Remove(policy);
+            Console.WriteLine("Policy deleted successfully!");
+        }
+
+        public void ViewActivePolicy()
+        {
+            foreach (var policy in policies)
+            {
+                if (policy.IsActive())
+                    Console.WriteLine(policy);
+            }
+        }
+
+    }
+}
