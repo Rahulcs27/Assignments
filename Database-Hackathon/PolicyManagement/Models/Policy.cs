07feacc01d34a3faaec32962d03457d37646cdfc
Rahul Suthar <90858008+Rahulcs27@users.noreply.github.com>
2025-03-11 17:37:52 +0530

    DatabaseConnectivity

---
+++ Database-Hackathon/PolicyManagement/Models/Policy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolicyManagement.Models
+{
+
+    public class Policy
+    {
+        public int PolicyId { get; set; }
+        public string PolicyHolderName { get; set; }
+        public string PolicyType { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
